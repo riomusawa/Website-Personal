9cf6f02ab21d06aea0069280a98209410d5a17c6
riomusawa <riomusawa03@gmail.com>
2025-03-20 14:07:14 +0700

    Update style.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function appendValue(value){
-    document.getElementById("display").value += value;
-}  
 
-function cleardisplay(){
-      document.getElementById("display").value = "";
-}
-
-function calculate(){
-    const display = document.getElementById("display");
-    try{
-        display.value = eval(display.value);
-    }catch{
-        display.value = "Error";
-    }
-} 
